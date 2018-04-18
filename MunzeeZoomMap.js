d66526017eca1cd06b7403331ba16b208ec5abd9
Jakub Jelen <jakuje@gmail.com>
2018-04-18 22:51:21 +0200

    Ugly working version for GM4

--- MunzeeZoomMap.js
+++ MunzeeZoomMap.js
@@ -1,26 +1,25 @@
 // ==UserScript==
 // @name           MunzeeZoomMap
 // @name:cs        MunzeeZoomMap
-// @namespace      Kaas.Greasemonkey.Munzee
+// @namespace      Jakuje.Greasemonkey.Munzee
 // @description    Enables you to zoom a Munzee map into a larger scale.
 // @description:cs Umožňuje zoomovat munzee mapu do většího měřítka.
 // @include        https://www.munzee.com/map*
-// @version        1.0.0
+// @version        2.0.0
 // @grant          none
+// @require        https://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
-jQuery(document).ready(function ($)
-{
+(function() {
   // original values:
   //  12 for "only vacant rooms"
   //  15 for "show circle"
   //  13 for everything else
   var newMinZoom = 8;
-  
-  _minZoom = newMinZoom;  // override default
-  the_map.setOptions({minZoom:  newMinZoom});
 
-  $('#check_vacant').change(function( ) { the_map.setOptions({minZoom:  newMinZoom}); });
-  $('#check_circles').change(function( ) { the_map.setOptions({minZoom:  newMinZoom}); });
-});
+  window.eval("_minZoom = " + newMinZoom);  // override default
+  window.eval("the_map.setOptions({minZoom: " + newMinZoom + "});"); // make it happen
 
+  window.eval("$('#check_vacant').change(function() { the_map.setOptions({minZoom: "+newMinZoom+"}); });");
+  window.eval("$('#check_circles').change(function(){ the_map.setOptions({minZoom: "+newMinZoom+"}); });");
+})();
